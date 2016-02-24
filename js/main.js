38a45f236d2955983ef7edaac04cf7f6638dd346
Matthew-K <makolodzi@gmail.com>
2016-02-24 13:10:22 -0500

    Replace placeholder info with info from current

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 var current = example_conditions.current_observation;
+
+
+//The following use the current object to get the appropriate weather info 
+
+$("h1").text(current.display_location.full);
+
+$("h2").text("Today");
+
+$("#temp").text(current.temp_f);
+
+$("#feelsLike").text(current.feelslike_f);
+
+$("#weather").text(current.weather);
+
+$("#weather_icon").attr("src", current.icon_url);
+
+$("#precip").text("Precipitation: " + current.precip_today_metric + "%");
+
+$("#humidity").text("Humidity: " + current.relative_humidity);
+
+$("#wind").text("Wind: " + current.wind_mph);
