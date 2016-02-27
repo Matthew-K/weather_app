130e2ff8ec84782711bb9240a74d435bba2a10e8
Matthew-K <makolodzi@gmail.com>
2016-02-27 12:00:27 -0500

    Put functions into MVC format, fix API issue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+/*==========   Model   ==========*/
+
 var data = {
 
+  //Current weather that will eventually have nulls replaced with info from API call
   current: {
     location: null,
     temp: null,
@@ -13,9 +16,9 @@ var data = {
 
   /*threeDay will eventually have four objects containing today's and the next three days' weather data*/
 
-  threeDay: [/*today, day1, day2, day3, day4*/]
+  threeDay: [/*today, tomorrow, day2, day3*/]
 
-  /*Each object inside threeDay will have the following info. Note: The following only shows the key/value pairs that will or might be used in displaying the three day forecast. It does not include all of the key/value pairs.
+  /*Each object inside threeDay will have the following info. Note: The following only shows the key/value pairs that will or might be used in displaying the three day forecast. There are more key/value pairs received from the API call that each object will have.
 
     "date": {
     "day": 26,
@@ -40,85 +43,115 @@ var data = {
 };
 
 
-function setCurrent(currentInfo){
-  data.current = {
-    location: currentInfo.display_location.full,
-    temp: currentInfo.temp_f,
-    feelsLike: currentInfo.feelslike_f,
-    conditions: currentInfo.weather,
-    icon: currentInfo.icon_url,
-    precipChance: currentInfo.precip_today_metric,
-    humidity: currentInfo.relative_humidity,
-    wind: currentInfo.wind_mph       
-  };
-}
 
+/*==========   Controller   ==========*/
 
-function setthreeDay(info){
-  var simpleForecast = info.simpleforecast.forecastday;
-  simpleForecast.forEach(function(val){
-    data.threeDay.push(val);
-  });
-}
+var controller = {
 
-var testingTesting = null;
+  //Runs when program first started. API will get called/data updated, view will get rendered, and button click handlers will be created
+  init: function(){
+    controller.getWeatherInfo();
+    view.init();
+    buttons.init();
+  },
 
-function getWeather(){
-  $.ajax({
-      type: 'GET',
-      url: "http://api.wunderground.com/api/" + private.key + "/forecast/conditions/q/MA/Boston.json",
-      success: function(info) {
-        console.log("----------------------------");
-        console.log("   Data from API received   ");
-        console.log("----------------------------");
-        testingTesting = info;
-        setthreeDay(info.forecast);
-        renderthreeDayForecast();
+  //Takes current weather info from the API call and sets it in data.current
+  setCurrent: function(currentInfo){
+    data.current = {
+      location: currentInfo.display_location.full,
+      temp: currentInfo.temp_f,
+      feelsLike: currentInfo.feelslike_f,
+      conditions: currentInfo.weather,
+      icon: currentInfo.icon_url,
+      precipChance: currentInfo.precip_today_metric,
+      humidity: currentInfo.relative_humidity,
+      wind: currentInfo.wind_mph       
+    };
+  },
 
-        var currentInfo = info.current_observation;
-        setCurrent(currentInfo);
-        renderCurrent();
-      }
+  //Takes three day forecast info from the API call and sets it in data.threeday
+  setthreeDay: function(info){
+    var simpleForecast = info.simpleforecast.forecastday;
+    simpleForecast.forEach(function(val){
+      data.threeDay.push(val);
     });
-}
+  },
 
+  /*Gets weather information from Wunderground API and uses it to set data.current and data.threeday*/
+  getWeatherInfo: function(){
+    $.ajax({
+        type: 'GET',
+        url: "http://api.wunderground.com/api/" + private.key + "/forecast/conditions/q/MA/Boston.json",
+        success: function(info) {
+          console.log("----------------------------");
+          console.log("   Data from API received   ");
+          console.log("----------------------------");
+          testingTesting = info;
+          controller.setCurrent(info.current_observation);
+          controller.setthreeDay(info.forecast);    
+        }
+      });
+  },
+
+  //Retrieves the current weather from data.current
+  getCurrentWeather: function(){
+    return data.current;
+  },
+
+  //Retrieves the three day forecast from data.threeDay
+  getThreeDayForecast: function(){
+    return data.threeDay;
+  }
+
+};
+
+
+
+/*==========   View   ==========*/
 
 var view = {
+
+  //Runs when program is first initialized
+  init: function(){
+    $(document).ajaxStop(function () {
+      view.renderCurrent(controller.getCurrentWeather());
+    });
+  },
+
   //Renders current weather 
-  renderCurrent: function(){
-    $("h1").text(data.current.location);
+  renderCurrent: function(currentWeather){
+    $("h1").text(currentWeather.location);
     $("h2").text("Today");
-    $("#temp").text(data.current.temp);
-    $("#feelsLike").text(data.current.feelsLike);
-    $("#weather").text(data.current.conditions);
-    $("#weather_icon").attr("src", data.current.icon);
-    $("#precip").text("Precipitation: " + data.current.precipChance + " %");
-    $("#humidity").text("Humidity: " + data.current.humidity);
-    $("#wind").text("Wind: " + data.current.wind);
+    $("#temp").text(currentWeather.temp);
+    $("#feelsLike").text(currentWeather.feelsLike);
+    $("#weather").text(currentWeather.conditions);
+    $("#weather_icon").attr("src", currentWeather.icon);
+    $("#precip").text("Precipitation: " + currentWeather.precipChance + " %");
+    $("#humidity").text("Humidity: " + currentWeather.humidity);
+    $("#wind").text("Wind: " + currentWeather.wind);
   },
 
   //Renders the three day forecast
-  renderthreeDayForecast: function(){
-    view.createThreeDayDivs(data.threeDay);
-    for (var i = 0; i < data.threeDay.length; i++){
+  renderthreeDayForecast: function(threeDayForecast){
+    view.createThreeDayDivs(threeDayForecast);
+    for (var i = 0; i < threeDayForecast.length; i++){
       view.addWeatherTags("#day" + i, i);
-      view.fillWeatherContent(data.threeDay[i], i);
+      view.fillWeatherContent(threeDayForecast[i], i);
     }
   },
 
-
-  //Creates four divs in order to show the three day forecast properly
+  /*Creates four divs in order to show the three day forecast properly (a seperate div for today(id="day0"), tomorrow(id="day1"), day 2(id="day2"), and day 3(id="day3"))*/
   createThreeDayDivs: function(days){
     for (var i = 0; i < days.length; i++){
         newDiv = document.createElement("div");
-        $(newDiv).attr("id","day"+ i).addClass('threeDay').appendTo("#threeDayTest");
+        $(newDiv).attr("id","day"+ i).addClass('threeDay').appendTo("#threeDayGrid");
     }  
   },
 
   /*
   addWeatherTags adds weather ready html tags and ids to each three day forecast div created by createThreeDayDivs. 
 
-  When entering the selector enter as you would when choosing a selector using jQuery. For instance, if the selector you want to has an id named dog, use "#dog" as the selector parameter. 
+  When entering the selector enter as you would when choosing a selector using jQuery. For instance, if the selector you want to has an id named day0, use "#day0" as the selector parameter. 
 
  
   Index corresponds to the index in the threeDay array. It will be used as a suffix for all the ids created by this function
@@ -131,9 +164,8 @@ var view = {
     $(selector).append("<img id =icon" + index +">");
   },
 
-  /*
-  fillWeatherContent is closely related to addWeatherTags. Enter the same index used for that function in this one to fill in the proper information inside the tags
-  */
+  
+  /*fillWeatherContent is closely related to addWeatherTags. Enter the same index used for that function in this one to fill in the proper information inside the html tags*/
   fillWeatherContent: function(day, index){
     if(index === 0){
       $("#weekday"+ index).text("Today " + day.date.month + '/' + day.date.day);
@@ -147,25 +179,38 @@ var view = {
   }
 };
 
-//init();
+
+buttons = {
+  //Creates Current button click handler
+  createCurrentClick: function(){
+    $("#currentButton").on("click", function(){
+      $("#current").show();
+      $("#threeDayGrid").hide();
+    });
+  },
+
+  //Creates Three Day Forecast button click handler 
+  createThreeDayClick: function(){
+    $("#test").on("click", function(){  
+      $("#current").hide();
+      if( $('#threeDayGrid').is(':empty') ) {
+        view.renderthreeDayForecast(controller.getThreeDayForecast());
+      }
+      $("#threeDayGrid").show();
+    });
+  },
+
+  //Runs when program first initialized. Creates button click handlers
+  init: function(){
+    buttons.createCurrentClick();
+    buttons.createThreeDayClick();
+  }
+};
+
+controller.init();
 
 
 
-$("#test").on("click", function(){  
-  $("#current").hide();
-  $("#threeDayTest").show();
-});
-
-$("#currentButton").on("click", function(){
-  $("#current").show();
-  $("#threeDayTest").hide();
-});
 
 
 
-$("#threeDayTest").hide();
-function init(){
-  getWeather();
-  //Explore why below has to be inside ajax function
-  //showCurrent();
-}
